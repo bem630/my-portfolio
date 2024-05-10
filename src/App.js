0ffde359da5d53f43148239c9988d9d3f40207f1
bem630 <bah70495@gmail.com>
2024-05-10 15:05:27 +0100

    ✨ F01: Add routing com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './component/pages/Navbar';
+import Home from './component/pages/Home';
+import Contact from './component/pages/Contact';
+import About from './component/pages/About';
+import Services from './component/pages/Services';
+import Projets from './component/pages/Projets';
+import ErrorPage from './component/pages/ErrorPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Salut</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/services' element={<Services/>}/>
+        <Route path='/projets' element={<Projets/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
+    </>
   );
 }
 
